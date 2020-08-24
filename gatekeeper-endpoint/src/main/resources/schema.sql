82e7d212ba0dd16916953a92907761e8b6b8765b
skocheri <skocheri@rubiconproject.com>
2020-08-24 10:36:59 -0700

    updating sql

--- gatekeeper-endpoint/src/main/resources/schema.sql
+++ gatekeeper-endpoint/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE session_domain (
 	id serial PRIMARY KEY,
 	session_id VARCHAR ( 50)  NOT NULL,
-	domain VARCHAR ( 50 ) NOT NULL,
+	domain VARCHAR ( 300 ) NOT NULL,
 	created_on TIMESTAMP NOT NULL
 );
 
